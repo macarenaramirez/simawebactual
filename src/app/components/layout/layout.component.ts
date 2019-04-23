d98e6db39e9940797224a9af70e48eea18ce4c5e
Gustavo <gustavoantunez@gmail.com>
2019-04-23 07:23:15 -0400

    Sass. Colores generales.

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -11,7 +11,7 @@ export class LayoutComponent implements OnInit {
   }
 
   ngOnInit() {
-    document.body.className = 'skin-blue';
+    document.body.className = 'skin-sima sidebar-mini';
   }
 
 }
